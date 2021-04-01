65b72c288eda8c6114312202668b6df93ae31dca
Bogdan Bodirlau <bodi_iulius@yahoo.com>
2021-04-01 10:45:46 +0300

    Making the commands component and creating a service for that components.

--- src/app/main-pages/queue/commands/commands.component.ts
+++ src/app/main-pages/queue/commands/commands.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommandsService } from 'src/app/shared/services/queue-commands.service'
+import { ICommands } from 'src/app/shared/interfaces/.interfaces';
+import { AllOrders } from 'src/app/shared/services/.services';
 
 @Component({
   selector: 'app-commands',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommandsComponent implements OnInit {
 
-  constructor() { }
+  @Input()index: number;
+  customer_name:string ='';
+  starting_point:string ='';
+  destination:string ='';
+  amount_profit:number ;
+  driver_name:string ='';
 
-  ngOnInit(): void {
+  constructor(
+    private commandsService: CommandsService,
+    private AllOrders: AllOrders
+  ) { }
+
+  // newCommand:ICommands = {
+  //   customer_name : '',
+  //   starting_point:'',
+  //   destination:'',
+  //   amount_profit: 0,
+  //   driver_name: '',
+  // }
+
+  ngOnInit(): void {    
+
+    this.customer_name = this.AllOrders.allOrders[this.index].customer_name;
+    this.starting_point = this.AllOrders.allOrders[this.index].starting_point;
+    this.destination = this.AllOrders.allOrders[this.index].destination;
+    this.amount_profit = this.AllOrders.allOrders[this.index].amount_profit
+    this.driver_name = this.AllOrders.allOrders[this.index].driver_name
+
+    // console.log(this.starting_point)
+    // this.newCommand.starting_point = this.commandsService.order.starting_point;
+    // this.starting_point = this.newCommand.starting_point;
+
+    // this.newCommand.destination = this.commandsService.order.destination;
+    // this.destination = this.newCommand.destination;
+
+    // this.newCommand.amount_profit = this.commandsService.order.amount_profit;
+    // this.amount_profit = this.newCommand.amount_profit;
+
+    // this.newCommand.driver_name = this.commandsService.order.driver_name;
+    // this.driver_name = this.newCommand.driver_name;
   }
 
 }
--- src/app/main-pages/queue/new-command/new-command.component.ts
+++ src/app/main-pages/queue/new-command/new-command.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, EventEmitter, Output } from '@angular/core';
+import { ICommands } from 'src/app/shared/interfaces/.interfaces';
+import { CommandsService } from 'src/app/shared/services/queue-commands.service';
+
 
 @Component({
   selector: 'app-new-command',
@@ -7,7 +10,41 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation:ViewEncapsulation.None
 })
 export class NewCommandComponent {
+  @Output() saveEmittor = new EventEmitter()
+  saveCommand: boolean;
   
-  value= 'Clear me';
+  customer_name:'';
+  starting_point:'';
+  destination:'';
+  amount_profit: 0;
+  driver_name: '';
 
+  newCommand:ICommands = {
+    customer_name : '',
+    starting_point:'',
+    destination:'',
+    amount_profit: 0,
+    driver_name: '',
+  }
+
+  constructor(private CommandsService: CommandsService){}
+
+  CommandSaved(){
+    this.newCommand.customer_name = this.customer_name;
+    this.newCommand.starting_point = this.starting_point;
+    this.newCommand.destination = this.destination;
+    this.newCommand.amount_profit = this.amount_profit;
+    this.newCommand.driver_name = this.driver_name;
+    
+    this.CommandsService.SaveNewCommand(this.newCommand)
+
+    this.customer_name = "";
+    this.starting_point = "";
+    this.destination = "";
+    this.amount_profit = null;
+    this.driver_name = "";
+
+    this.saveEmittor.emit(this.saveCommand=true)
+  }
+  
 }
