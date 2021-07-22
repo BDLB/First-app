1c8c1bf668ff11404851caed9e0b013ede89ab71
Bogdan Bodirlau <bodi_iulius@yahoo.com>
2021-07-22 07:24:11 +0300

    Renaming Page Frame Tab Component.

--- src/app/shared/components/page-frame-sidenav/page-frame-tab.component.ts
+++ src/app/shared/components/page-frame-sidenav/page-frame-tab.component.ts
@@ -2,22 +2,26 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 
 import { PageFrameSidenavService } from '../../services/page-frame-sidenav.service';
-import { ThrowStmt } from '@angular/compiler';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-page-frame-sidenav',
-  templateUrl: './page-frame-sidenav.component.html',
-  styleUrls: ['./page-frame-sidenav.component.scss']
+  selector: 'app-page-frame-tab',
+  templateUrl: './page-frame-tab.component.html',
+  styleUrls: ['./page-frame-tab.component.scss']
 })
-export class PageFrameSidenavComponent implements OnInit {
+export class PageFrameTabComponent implements OnInit {
 
   constructor(
     private _location: Location,
+    private _router: Router,
+    private route: ActivatedRoute,
     private _pageFrameSidenav: PageFrameSidenavService
   ) { }
   openSidenav: boolean;
   @Input() tabs: string[];
   currentTab: string;
+  URL: any;
+  private sub: any;
 
   ngOnInit(): void {
     this._pageFrameSidenav.sidenavSwitch.subscribe(
@@ -31,9 +35,11 @@ export class PageFrameSidenavComponent implements OnInit {
   }
 
   changeSidenavPage(tabPressed) {
-    this.currentTab = tabPressed
-    if (this.currentTab = "Drivers") {
-      
+    this.URL = window.location.pathname
+    tabPressed = tabPressed.toLowerCase();
+    if (!this.URL.includes(tabPressed)){
+      var url = this.URL.split("/")
+      this._router.navigate([`./../../sidenav/${tabPressed}/${url[4]}`])
     }
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
-import { SidenavDriverComponent } from '../main-pages/queue/sidenavs/sidenav-driver/sidenav-driver.component';
-import { SidenavDriversComponent } from '../main-pages/queue/sidenavs/sidenav-drivers/sidenav-drivers.component';
-import { MaterialSharedTableComponent } from './components/material-shared-table/material-shared-table.component';
-import { MaterialSharedTextColumnComponent } from './components/material-shared-text-column/material-shared-text-column.component';
 import {MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { PageFrameSidenavComponent } from './components/page-frame-sidenav/page-frame-sidenav.component';
-
+import { MaterialSharedTextColumnComponent } from './components/material-shared-text-column/material-shared-text-column.component';
+import { MaterialSharedTableComponent } from './components/material-shared-table/material-shared-table.component';
+import { SidenavDriversComponent } from '../main-pages/queue/sidenavs/sidenav-drivers/sidenav-drivers.component';
+import { SidenavDriverComponent } from '../main-pages/queue/sidenavs/sidenav-driver/sidenav-driver.component';
+import { PageFrameTabComponent } from './components/page-frame-sidenav/page-frame-tab.component';
 
 const routes: Routes = [
     { path: 'sidenav/drivers/:driver', component: SidenavDriversComponent },
@@ -21,21 +20,21 @@ const routes: Routes = [
     declarations: [
         MaterialSharedTextColumnComponent,
         MaterialSharedTableComponent,
+        SidenavDriversComponent,
         SidenavDriverComponent,
-        PageFrameSidenavComponent,
-        SidenavDriversComponent
+        PageFrameTabComponent,
     ],
     imports: [
         RouterModule.forChild(routes),
-        CommonModule,
-        MatTabsModule,
         MatSidenavModule,
-        MatTableModule
+        MatTableModule,
+        MatTabsModule,
+        CommonModule,
     ],
     exports: [
-        PageFrameSidenavComponent,
         MaterialSharedTextColumnComponent,
-        MaterialSharedTableComponent
+        MaterialSharedTableComponent,
+        PageFrameTabComponent,
     ]
 })
 export class SharedModule {}
